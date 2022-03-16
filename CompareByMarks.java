5112d5fefb914d67783a47827d3399781ee72ea4
SanjanaMR <sanjanamr6872@gmail.com>
2022-03-16 09:01:13 +0530

    comparebymarks

---
+++ CompareByMarks.java
@@ -0,0 +1,13 @@
+package com.te.javabasics.collection;
+
+import java.util.Comparator;
+
+public class CompareByMarks implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+
+		return o1.marks - o2.marks;
+	}
+
+}
